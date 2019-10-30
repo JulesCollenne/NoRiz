361e5d0680ba9b9427f94ea0b205cd50985caaf7
JulesCollenne <D372e3b9206>
2019-10-30 18:26:26 +0100

    Organisation des classes avant de programmer

--- src/main/java/Strategy/AngleStrat.java
+++ src/main/java/Strategy/AngleStrat.java
@@ -1,5 +1,17 @@
 package Strategy;
 
+/**
+ * 1.Soit x l'angle entre Player et le Monster
+ * Et D,R,U,L les directions :
+ *
+ *     | cos(x)         | sin(x)
+ *   D | -0.5 < A < 0.5 |  A < 0
+ *   R | A > 0          | -0.5 < A < 0.5
+ *   U | -0.5 < A < 0.5 |  A > 0
+ *   L | A < 0          | -0.5 < A < 0.5
+ *
+ *
+ */
 public class AngleStrat implements Strategy {
     public int newWay() {
         return 0;
