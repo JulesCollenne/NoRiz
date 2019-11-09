cf69c8f71423f5419c2db4cd3772f3e200202c2e
JulesCollenne <jules.collenne@etu.univ-amu.fr>
2019-11-09 14:38:33 +0100

    Collisions apparement corrigées

--- src/main/java/States/StartMenuState.java
+++ src/main/java/States/StartMenuState.java
@@ -10,6 +10,9 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.*;
 
 public class StartMenuState extends GameState{
+
+    boolean firstRender = true;
+
     StartMenuState(GameStateManager gsm) {
         super(gsm);
     }
@@ -26,40 +29,37 @@ public class StartMenuState extends GameState{
     @Override
     public void render(GraphicsContext gc) {
 
-        Pane layout = new Pane();
+        if(firstRender) {
 
-        layout.setStyle("-fx-background-color: darkslategrey;");
+            Pane layout = new Pane();
 
-        Button easy = new ImageButton(new Image("sign_facile.png"), "Facile");
-        easy.setStyle("-fx-background-color: darkslategrey;");
-        easy.setLayoutX(165-(230/2));
-        easy.setLayoutY(495-(87.5/2));
-        easy.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                System.out.println("Mode facile");
-            }
-        });
+            layout.setStyle("-fx-background-color: darkslategrey;");
 
-        Button medium = new ImageButton(new Image("sign_moyen.png"), "Medium");
-        medium.setStyle("-fx-background-color: darkslategrey;");
-        medium.setLayoutX(500-(230/2));
-        medium.setLayoutY(495-(87.5/2));
+            Button easy = new ImageButton(new Image("sign_facile.png"), "Facile");
+            easy.setStyle("-fx-background-color: transparent;");
+            easy.setLayoutX(165 - (230 / 2));
+            easy.setLayoutY(495 - (87.5 / 2));
+            easy.setOnAction(actionEvent -> System.out.println("Mode facile"));
 
-        Button hard = new ImageButton(new Image("sign_difficile.png"), "Hard");
-        hard.setStyle("-fx-background-color: darkslategrey;");
-        hard.setLayoutX(830-(230/2));
-        hard.setLayoutY(495-(87.5/2));
+            Button medium = new ImageButton(new Image("sign_moyen.png"), "Medium");
+            medium.setStyle("-fx-background-color:transparent");
+            medium.setLayoutX(500 - (230 / 2));
+            medium.setLayoutY(495 - (87.5 / 2));
+
+            Button hard = new ImageButton(new Image("sign_difficile.png"), "Hard");
+            hard.setStyle("-fx-background-color: darkslategrey;");
+            hard.setLayoutX(830 - (230 / 2));
+            hard.setLayoutY(495 - (87.5 / 2));
 
 
-        layout.getChildren().addAll(easy, medium, hard);
+            layout.getChildren().addAll(easy, medium, hard);
 
+            Scene scene = new Scene(layout, 1000, 1000);
 
-
-        Scene scene = new Scene(layout, 1000, 1000);
-
-        gsm.theStage.setScene(scene);
-        gsm.theStage.show();
+            gsm.theStage.setScene(scene);
+            gsm.theStage.show();
+            firstRender = false;
+        }
 
     }
 }
