ac36af1d60b2b93a1c9ac2982a98d830d0284b9c
Anthony <gerz>
2019-11-13 09:51:27 +0100

    v2

--- src/main/java/UI/inGameUserInterface.java
+++ src/main/java/UI/inGameUserInterface.java
@@ -7,9 +7,9 @@ import javafx.scene.canvas.GraphicsContext;
 
 public class inGameUserInterface {
 
-    GameStateManager gsm;
+    private GameStateManager gsm;
 
-    public static long roundTimer = Utils.roundDuration; // en seconde
+    private static long roundTimer = Utils.roundDuration; // en seconde
     public static long startTimer = 0;
 
     public static int currentNbRice = 0;
@@ -20,60 +20,62 @@ public class inGameUserInterface {
 
     }
 
-    public static void reset(){
+    public void init(){
 
         startTimer = 0;
-        currentNbRice = 0;
+
+        switch(gsm.difficulty){
+
+            case EASY:
+                currentNbRice = matrixWorld.nbRiceWorld1;
+                break;
+
+            case MEDIUM:
+                currentNbRice = matrixWorld.nbRiceWorld2;
+                break;
+
+            case HARD:
+                currentNbRice = matrixWorld.nbRiceWorld3;
+                break;
+
+            default:
+                currentNbRice = matrixWorld.nbRiceWorld1;
+                break;
+
+        }
 
     }
 
-    public void renderTimer(GraphicsContext gc){
+    public void render(GraphicsContext gc){
+
+        renderTimer(gc);
+        renderNbRice(gc);
+
+    }
+
+    private void renderTimer(GraphicsContext gc){
 
         long timer;
         long currentTimer;
 
         currentTimer = System.nanoTime();
 
-        timer = Math.abs((currentTimer/1000000000) - (inGameUserInterface.startTimer/1000000000));
+        timer = Math.abs((currentTimer/1000000000) - (startTimer/1000000000));
 
-        if(timer > inGameUserInterface.roundTimer) {
-            inGameUserInterface.reset();
+        if(timer > roundTimer) {
             gsm.changeState(3);
         }
 
-        if(inGameUserInterface.roundTimer - timer >= 100)
-            gc.fillText(inGameUserInterface.roundTimer - timer+"",Utils.canvasSize-(Utils.caseDimension+ Utils.caseDimension/2.0), 18+((20/100.0)*Utils.caseDimension));
+        if(roundTimer - timer >= 100)
+            gc.fillText(roundTimer - timer+"",Utils.canvasSize-(Utils.caseDimension+ Utils.caseDimension/2.0), 18+((20/100.0)*Utils.caseDimension));
         else
-            gc.fillText(inGameUserInterface.roundTimer - timer+"",Utils.canvasSize-(Utils.caseDimension), 18+((20/100.0)*Utils.caseDimension));
+            gc.fillText(roundTimer - timer+"",Utils.canvasSize-(Utils.caseDimension), 18+((20/100.0)*Utils.caseDimension));
 
     }
 
-    public void setNbRice(){
+    private void renderNbRice(GraphicsContext gc){
 
-        switch(gsm.difficulty){
-
-            case EASY:
-                inGameUserInterface.currentNbRice = matrixWorld.nbRiceWorld1;
-                break;
-
-            case MEDIUM:
-                inGameUserInterface.currentNbRice = matrixWorld.nbRiceWorld2;
-                break;
-
-            case HARD:
-                inGameUserInterface.currentNbRice = matrixWorld.nbRiceWorld3;
-                break;
-
-            default:
-                inGameUserInterface.currentNbRice = matrixWorld.nbRiceWorld1;
-                break;
-
-        }
-    }
-
-    public void renderNbRice(GraphicsContext gc){
-
-        gc.fillText(inGameUserInterface.currentNbRice+"",(10/100.0)*Utils.caseDimension, 18+(10/100.0)*Utils.caseDimension);
+        gc.fillText(currentNbRice+"",(10/100.0)*Utils.caseDimension, 18+(10/100.0)*Utils.caseDimension);
 
     }
 
