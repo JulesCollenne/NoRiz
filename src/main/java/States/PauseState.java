fb8f7e39a87eb2ad4f70553964de5005b11afd69
thomas <thomas.foucour@gmail.com>
2019-11-17 05:01:38 -0800

    Pause : voir pour le skin des boutons de commande

--- src/main/java/States/PauseState.java
+++ src/main/java/States/PauseState.java
@@ -1,7 +1,15 @@
 package States;
 
+import Utils.Utils;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 public class PauseState extends GameState {
     PauseState(GameStateManager gsm) {
@@ -24,6 +32,33 @@ public class PauseState extends GameState {
 
     @Override
     public void initScene() {
+        Pane layout = new Pane();
+
+        layout.setStyle("-fx-background-color: darkslategrey;");
+
+
+        Text title = new Text("Pause");
+        title.setX(Utils.canvasSize/2- 75);
+        title.setY(Utils.canvasSize/2 - 200);
+        title.setFont(new Font(40));
+        title.setStyle("-fx-font-weight: bold");
+
+        Text commande = new Text("Commandes");
+        commande.setX(Utils.canvasSize/9);
+        commande.setY(Utils.canvasSize/2);
+        commande.setFont(new Font(20));
+
+        ModeButton rejouer = new ModeButton(new Image("Buttons/sign_rejouer.png"), "Rejouer");
+        rejouer.setLayoutX(Utils.canvasSize/2);
+        rejouer.setLayoutY(Utils.canvasSize/2);
+        rejouer.setMaxSize((20 /100.0)*Utils.canvasSize,(10 /100.0)*Utils.canvasSize);
+        rejouer.handler(gsm);
+
+
+        layout.getChildren().addAll(title, commande, rejouer);
+
+
+        theScene = new Scene(layout, Utils.canvasSize, Utils.canvasSize);
 
     }
 }
