7aec65dad6cc9dba25fef9535c5fd830ef178fb1
Floriangarcia7 <f.garcia13@hotmail.fr>
2019-11-20 19:28:15 +0100

    Les monstres dans un Thread

--- src/main/java/States/GameStateManager.java
+++ src/main/java/States/GameStateManager.java
@@ -14,7 +14,7 @@ import javafx.stage.Stage;
 
 import static Utils.Utils.*;
 
-public class GameStateManager {
+public class GameStateManager{
 
     private int currentState = START;
     private GameState gameStates[] = new GameState[5];
@@ -25,10 +25,11 @@ public class GameStateManager {
 
     Collider collider = new Collider(world);
     public Player player = new Player(collider, caseDimension,caseDimension*3,1);
-    Monster[] monsters = new Monster[4];
     public BonusItem[] bonuses = new BonusItem[2];
 
     SoundManager sm = new SoundManager();
+    Monster[] monsters = new Monster[4];
+    private ThreadMonsters threadMonsters;
 
     public enum DIF {EASY, MEDIUM, HARD}
 
@@ -40,7 +41,9 @@ public class GameStateManager {
 
         world.build(difficulty);
 
-        createMonsters();
+        //createMonsters();
+        threadMonsters = new ThreadMonsters(player,collider,world,monsters);
+        threadMonsters.start();
         createBonuses();
 
         inGameUserInterface ui = new inGameUserInterface(this);
@@ -87,17 +90,34 @@ public class GameStateManager {
 
     }
 
-    /**
-     * Create 4 monsters
-     */
+    static class ThreadMonsters extends Thread{
+        Monster[] monsters;
+        Player player;
+        Collider collider;
+        World world;
 
-    private void createMonsters(){
-        //Coordonnée de départ dans le cas de notre map test: Faire en sorte que les coordonnés de départ correspondent au niveaux dans lequel on est
-        monsters[0] = new Monster(10 * caseDimension, 10 * caseDimension + (2*caseDimension), 1, new AngleStrat(player), "Cat hamaran", collider);                                               //Monstre AngleStrat
-        monsters[1] = new Monster(10 * caseDimension, 11 * caseDimension + (2*caseDimension), 1, new RandomStrat(), "Cat holik", collider);                                                   //Monstre RandomStrat
-        monsters[2] = new Monster(10 * caseDimension, 10 * caseDimension + (2*caseDimension), 1, new BonusStrat(world), "Cat o'dick", collider);                                                //Monstre BonusStrat
-        monsters[3] = new Monster(10 * caseDimension, 10* caseDimension + (2*caseDimension), 1, new HalfRandomStratHalfAngleStrat(player), "Cat reuh", collider);       //Monstre BonusStrat
+        public ThreadMonsters(Player player, Collider collider, World world, Monster[] monsters){
+            this.player = player;
+            this.collider = collider;
+            this.world = world;
+            this.monsters = monsters;
+        }
+
+        /**
+         * Create 4 monsters
+         */
+
+        private void createMonsters(){
+            //Coordonnée de départ dans le cas de notre map test: Faire en sorte que les coordonnés de départ correspondent au niveaux dans lequel on est
+            monsters[0] = new Monster(10 * caseDimension, 10 * caseDimension + (2*caseDimension), 1, new AngleStrat(player), "Cat hamaran", collider);                                              //Monstre AngleStrat
+            monsters[1] = new Monster(10 * caseDimension, 11 * caseDimension + (2*caseDimension), 1, new RandomStrat(), "Cat holik", collider);                                                     //Monstre RandomStrat
+            monsters[2] = new Monster(10 * caseDimension, 10 * caseDimension + (2*caseDimension), 1, new BonusStrat(world), "Cat o'dick", collider);                                                //Monstre BonusStrat
+            monsters[3] = new Monster(10 * caseDimension, 10* caseDimension + (2*caseDimension), 1, new HalfRandomStratHalfAngleStrat(player), "Cat reuh", collider);                               //Monstre Half
+        }
+
+        public void run(){
+            createMonsters();
+        }
     }
-
 }
 
