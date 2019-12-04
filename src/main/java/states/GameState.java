2ffcdc3a540b679e28caada89ae5a3beee31cf80
Sim-Pirs <theazx05@gmail.com>
2019-12-04 18:31:53 +0100

    vrai skins des bonus

--- src/main/java/states/GameState.java
+++ src/main/java/states/GameState.java
@@ -37,7 +37,7 @@ public abstract class GameState {
                 long elapsedNanos = currentNanoTime - lastTime ;
                 System.out.println("FPS : " + 1000000000. / elapsedNanos);
 */
-                if(timer % 2 == 0){
+                if(timer % 4 == 0){
                     timer++;
                     return;
                 }
