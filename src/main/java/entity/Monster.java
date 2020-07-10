32394ead6e2a041a4f2aa2542477a64d46dcacf2
Jules <jules.collenne@gmail.com>
2020-07-10 15:35:49 +0200

    Test4
    Signed-off-by: Jules <jules.collenne@gmail.com>

--- src/main/java/entity/Monster.java
+++ src/main/java/entity/Monster.java
@@ -105,7 +105,7 @@ public class Monster extends Entity{
                 facing = nextFacing;
                 nextFacing = DIRECTION.STOP;
             }
-            //test
+            //test0
 
             switch (facing) {
                 case DOWN -> tryMove(0, speed);
