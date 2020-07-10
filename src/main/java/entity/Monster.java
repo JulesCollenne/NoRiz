20eea4ea3186fa8e256cf1115fd208383016f185
Jules <jules.collenne@gmail.com>
2020-07-10 15:32:39 +0200

    Test

--- src/main/java/entity/Monster.java
+++ src/main/java/entity/Monster.java
@@ -105,6 +105,7 @@ public class Monster extends Entity{
                 facing = nextFacing;
                 nextFacing = DIRECTION.STOP;
             }
+            //test
 
             switch (facing) {
                 case DOWN -> tryMove(0, speed);
